f554362f28acb521e05f03f2df32f8764bb262b3
Dawid Rolinski <75127768+r0lo16@users.noreply.github.com>
2022-11-15 12:44:04 +0100

    add csv and sorting best solutions (#8)
    * test
    
    Dodanie cvs
    
    * add_packages
    
    Add packages pandas, sorting best_solutions
    
    * restor
    
    restore

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from cmath import inf
 from dis import dis
 from math import dist
+from timeit import timeit
 from tkinter.tix import Select
 from loader import load_locations_from_file, calculate_distances_matrix
 from solvers import initialize_solution, InitializationStrategy, Individual
 from configparser import ConfigParser
 from functools import partial
 import random
+import time
 from solvers import initialize_population, evaluate, selection, crossover, mutation
 from solvers.genetic_algorithms import CrossoverStrategy, MutationStrategy, SelectionStrategy
 from tqdm import tqdm
+import pandas as pd
+
+import csv
+import locale
+
+start_time = time.time()
 
 def main():
+
     configur = ConfigParser()
     configur.read("config.ini")
     init_strategy = InitializationStrategy.convert(configur.get("ea", "init_strategy"))
@@ -39,12 +48,50 @@ def main():
             P2 = selection(populations[current_population_index], selection_strategy, tour_size)
             O1 = crossover(P1, P2, crossover_strategy, crossover_probability, distances)
             O1 = mutation(O1, mutation_strategy, mutation_probability, distances)
-            #evaluate(O1) #TODO
+            # evaluate(O1) #TODO
             populations[current_population_index + 1].append(O1)
             if O1.cost < best_solution.cost:
                 best_solution = O1
 
+    end_time = time.time()
+    finish_time= end_time-start_time
     print(f"Best solution: {best_solution.cost}")
+    print(f"Strategy: {crossover_strategy}")
+
+    print(f"data_set:  {data_set}")
+    print(f"max generations:  {max_generations}")
+    print(f"start_city:  {start_city}")
+    print(f"time:  {finish_time}")
+
+    # csvPath = "plik.csv"
+    # with open(csvPath) as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     # totalRecord =len(list(csv_reader1))
+    #     line_count = 0
+    #     for row in csv_reader:
+    #         if line_count == 0:
+    #             print(f'Column names: {", ".join(row)}')
+    #             line_count += 1
+    #
+    #         else:
+    #             if row:
+    #                 print(f'\t{row[0]}, {row[1]},  {row[2]}, {row[3]}.')
+    #                 line_count += 1
+    #     print(f'Processed {line_count} lines.')
+
+
+    with open("berkin52_cross.csv", 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([f"{data_set}" , f"{best_solution.cost}", f"{max_generations}", f"{crossover_strategy}", f"{finish_time}"])
+
+
+    filecsv = pd.read_csv('berkin52_cross.csv')
+    print("Number of lines present:",len(filecsv))
+
+    sorted_filecsv = filecsv.sort_values(by=["Best solution"], ascending=False)
+    sorted_filecsv.to_csv('berkin52_cross.csv', index=False)
+
+
 
 
 if __name__ == "__main__":
